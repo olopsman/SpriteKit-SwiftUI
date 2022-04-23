f07768742e90960ebeeb495df291dd1278f5b109
Paulo Orquillo <olopsman@users.noreply.github.com>
2022-04-23 12:19:40 +1200

    Initial Commit

---
+++ SpriteKit+SwiftUI/SpriteKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_SwiftUIApp.swift
+//  SpriteKit+SwiftUI
+//
+//  Created by Paulo Orquillo on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
